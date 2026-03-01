4ce325eda0f20712c2518639c80031f6fd0eb385
Kem4yn <ubaydullaevgayrat1@gmail.com>
2026-03-01 13:20:06 +0300

    docs: finalized production readiness report and linting configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,8 @@ module.exports = {
       ],
       rules: {
         '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-extraneous-class': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
   ],
